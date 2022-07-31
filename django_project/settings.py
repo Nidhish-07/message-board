01b62d4d9fe392ea16d2ebab8b71aaabe1650b91
r3X <rch246chauhan@gmail.com>
2022-07-31 17:19:15 +0530

    heroku setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0sqh188&0cscen#2vl^)xtvq%u$s)g26-3+^!h8#b$1+%rh3^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
